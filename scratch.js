9d164b2fe749c5cf994627ab4f2b860fc5e2ad22
kartikeya-dimri <kartikeya.dimri@iiitb.ac.in>
2026-05-01 23:09:17 +0530

    remove scratch.js

--- scratch.js
+++
@@ -1,6 +0,0 @@
-import { generatePayload, logicalSize } from './client/payloads/generator.js';
-
-for (const size of [512, 1024]) {
-  const arr = generatePayload('array', size);
-  console.log(`Size: ${size}, logical: ${logicalSize(arr)}, json: ${JSON.stringify(arr).length}, elems: ${arr.length}`);
-}
